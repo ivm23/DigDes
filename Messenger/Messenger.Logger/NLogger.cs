a09ae9eb44917b773349f41785acf8cd20084e9a
ivm23 <marina200964@yandex.ru>
2017-10-28 10:11:11 +0300

    change NLogger.config

--- Messenger/Messenger.Logger/NLogger.cs
+++ Messenger/Messenger.Logger/NLogger.cs
@@ -1,28 +1,9 @@
 ﻿using NLog;
-using System;
-using System.Net;
-using System.Net.Http;
-using System.Threading;
-using System.Threading.Tasks;
-//using System.Web.Http.Controllers;
-//using System.Web.Http.Routing;
 
 namespace Messenger.Logger
 {
     public static class NLogger
     {
-        private static bool _isLoggerCreated = false;
-        private static NLog.Logger _logger;
-
-        public static NLog.Logger Logger
-        {
-            get
-            {
-                if (_isLoggerCreated)
-                   return _logger;
-                _logger = LogManager.GetCurrentClassLogger();
-                return LogManager.GetCurrentClassLogger();
-            }
-        }
+        public static NLog.Logger Logger = LogManager.GetCurrentClassLogger();
     }
 }
